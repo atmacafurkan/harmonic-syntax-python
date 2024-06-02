9fd2260c11b0c5e01392a6b8edce9ad66f3467f7
Furkan Atmaca <furkanatmaca@outlook.com.tr>
2024-06-02 11:23:04 +0300

    merge, label, and empty agreement constraints are added. Interface is developing.

--- harmonic-syntax.py
+++ harmonic-syntax.py
@@ -2,7 +2,8 @@
 from anytree import Node, RenderTree, AsciiStyle
 import csv
 from typing import List
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QPushButton, QDialog
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QPushButton, QDialog, QLabel, QFileDialog, QGridLayout, QTableWidget, QTableWidgetItem
+from PyQt5.QtSvg import QSvgWidget
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from anytree.exporter import UniqueDotExporter
 import sys
@@ -13,17 +14,18 @@ agree_dict = {'case_agr': 0, 'wh_agr' : 0, 'foc_agr' : 0}
 neutral_dict = {'case': 0, 'wh' : 0, 'foc' : 0}
 empty_dict = {'case_mt': 0, 'wh_mt': 0, 'foc_mt': 0}
 used_feats_dict = {'case': 0, 'wh' : 0, 'foc' : 0}
-constraints_dict = {'label_cons': 1, 'case_agr': 0, 'wh_agr' : 0, 'foc_agr' : 0, 'case': 0, 'wh' : 0, 'foc' : 0, 'case_mt' : 0, 'wh_mt': 0, 'foc_mt': 0}
+constraints_dict = {'merge_cond': 0, 'label_cons': 0, 'case_agr': 0, 'wh_agr' : 0, 'foc_agr' : 0, 'case': 0, 'wh' : 0, 'foc' : 0, 'case_mt' : 0, 'wh_mt': 0, 'foc_mt': 0}
 
 # Custom node class with a named list field
 class SyntaxNode(Node):
-    def __init__(self, name, label = None, merge_feat = None, neutral_feats = None, empty_agr = None, agree_feats = None, parent = None, children = None):
+    def __init__(self, name, label = None, merge_feat = None, neutral_feats = None, empty_agr = None, result_feats = None, agree_feats = None, parent = None, children = None):
         super(SyntaxNode, self).__init__(name, parent, children)
         self.label = label if label is not None else None
         self.merge_feat = merge_feat if merge_feat is not None and merge_feat != '' else None
         self.agree_feats = agree_feats if agree_feats is not None else agree_dict
         self.neutral_feats = neutral_feats if neutral_feats is not None else neutral_dict
         self.empty_agr = empty_agr if empty_agr is not None else empty_dict
+        self.result_feats = result_feats if result_feats is not None else constraints_dict
         self.other_nodes = []
 
     def add_other_node(self, other_node):
@@ -45,7 +47,7 @@ class SyntaxNode(Node):
 
     def evaluate_constraints(self, encountered_nodes=None, default_state = None):
         # Initialize a result dictionary for both agree_feats and neutral_feats
-        result_feats = constraints_dict.copy() if default_state is None else default_state
+        result_feats = constraints_dict.copy() #if default_state is None else default_state
 
         # Initialize encountered_nodes if not provided
         encountered_nodes = set() if encountered_nodes is None else encountered_nodes
@@ -62,19 +64,11 @@ class SyntaxNode(Node):
 
         # neutral feature violations
         for key, value in self.neutral_feats.items():
-            result_feats[key] = int(value) * domination_count
-
-        # for empty agree violations
-        for key, value in self.empty_agr.items():
             result_feats[key] = int(value) * domination_count     
 
         # If encountered before, reset evaluation
         if self.name in encountered_nodes:
             result_feats = constraints_dict.copy()
-
-        # Add labelling constraint # TURN INTO A SEPARATE FUNCTION!!!
-        if self.name:
-            result_feats['label_cons'] = 0
         
         # Recursive call for each child node
         for child in self.children:
@@ -87,16 +81,47 @@ class SyntaxNode(Node):
 
         return result_feats
     
-    def merge_condition(self):
-        merge_condition_results = {'merge_cond': 0}
-        if len(self.children) > 1:
-            if self.children[0].merge_feat and self.children[0].merge_feat != self.children[1].label:
-                merge_condition_results['merge_cond'] += 1
+    # function to draw linear representation of the tree
+    def to_linear(self):
+        if not self:
+            return ""
+        
+        if self.name:
+            my_name = str(self.name)
+        else:
+            my_name = ".."
 
-            if self.children[1].merge_feat and self.children[1].merge_feat != self.children[0].label:
-                merge_condition_results['merge_cond'] += 1 
+        result = "[" + my_name
+        if self.children:
+            result += " " + " ".join(child.to_linear() for child in self.children)
+        result += "]"
+    
+        return result
+        
+    # merge condition, only checked at the root node (latest operation)
+def merge_condition(node):
+    violation = 0
+    if len(node.children) > 1:
+        if node.children[0].merge_feat and node.children[0].merge_feat != node.children[1].label:
+            violation += 1
 
-        return merge_condition_results
+        if node.children[1].merge_feat and node.children[1].merge_feat != node.children[0].label:
+            violation += 1
+    return violation
+
+# for labelling constraint, only checked at the root node (latest operation)
+def label_constraint(node):
+    if node.name:
+        return 0
+    else:
+        return 1
+
+    # for empty agreement, only checked at the root node (since Agree would be the latest operation)
+def empty_agreement(node, result_dict):
+    empty_dict = node.empty_agr
+    for key, value in empty_dict.items():
+        result_dict[key] = int(value)
+    return result_dict
     
 # clone a node to avoid multiple assignment
 def clone_tree(node):
@@ -109,7 +134,8 @@ def clone_tree(node):
         merge_feat = node.merge_feat,
         agree_feats=node.agree_feats,
         empty_agr=node.empty_agr,
-        neutral_feats=node.neutral_feats
+        neutral_feats=node.neutral_feats,
+        result_feats = node.result_feats
         # Add other attributes as needed
     )
     for child in node.children:
@@ -150,11 +176,11 @@ def read_nodes_csv(csv_file_path: str) -> List[SyntaxNode]:
         for row in reader:
             node = SyntaxNode(
                 name=row['it'],
-                merge_feat=(row['mc']),
+                merge_feat= str(row['mc']),
                 agree_feats=parse_feats(row['ac']),
                 neutral_feats=parse_feats(row['ft']),
                 empty_agr = None,
-                label=row['lb']
+                label= str(row['lb'])
             )
             nodes.append(node)
 
@@ -251,13 +277,9 @@ def Label(my_node):
     return(my_nodes)
 
 # Agree function, only under sisterhood
-def Agree(my_node):
-    # Check if the node has sisters and no label
-    # if len(my_node.children) < 2:
-    #     return []
-    
+def Agree(my_node):    
     # empty agreement
-    if len(my_node.name)>0:
+    if len(my_node.name) > 0:
         new_node = clone_tree(my_node)
         new_node.other_nodes = my_node.other_nodes
 
@@ -307,14 +329,6 @@ def proceed_cycle(my_node):
 
     return output_nodes
 
-# import numeration
-my_nodes = read_nodes_csv("./unaccusative_numeration.csv")
-my_trial = Label(Merge(Label(Merge(my_nodes[0])[0])[0])[0])[0]
-
-# visualise
-for pre, _, node in RenderTree(my_nodes[0], style=AsciiStyle()):
-    print(f"{pre}{node.name} - {node.agree_feats} - {node.neutral_feats} - {node.domination_count()}")
-
 # Function to generate SVG content in memory
 def generate_svg_content(root_node):
     # Create a UniqueDotExporter
@@ -339,22 +353,87 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         # Set the title and size of the main window
-        self.setWindowTitle("Tree Visualization")
-        self.setGeometry(100, 100, 400, 200)  # Adjusted main window size
+        self.setWindowTitle("Harmonic Syntax Tabulator")
 
         # Create a central widget and a layout for it
-        central_widget = QWidget()
-        self.setCentralWidget(central_widget)
-        layout = QVBoxLayout(central_widget)
+        layout = QGridLayout()
 
-        # Create a button to trigger tree display
-        self.button = QPushButton("Click here to show the tree")
-        self.button.clicked.connect(self.show_tree_popup)
-        layout.addWidget(self.button)
+        # selecting the numeration
+        self.selected_numeration = QLabel('Selected Numeration: ')
+        self.select_numeration = QPushButton('Select Numeration')
+        self.select_numeration.clicked.connect(self.import_numeration)
+        layout.addWidget(self.selected_numeration,1,0)
+        layout.addWidget(self.select_numeration,0,0)
+
+
+        # displaying the input tree
+        self.input_tree = QSvgWidget(self)
+        layout.addWidget(self.input_tree,2,0)
+
+        # displaying eval
+        # Create a QTableWidget
+        self.table_eval = QTableWidget(self)
+        self.table_eval.setColumnCount(len(constraints_dict) + 1)
+        self.headers = list(constraints_dict.keys())
+        layout.addWidget(self.table_eval, 0, 1)
+
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+    def import_numeration(self):
+        self.numeration_path, _ = QFileDialog.getOpenFileName(self, 'Select Numeration', '.', 'Csv Files (*.csv)')
+        self.numeration = read_nodes_csv(self.numeration_path)
+        if self.numeration_path:
+            # change the selected filename
+            self.selected_numeration.setText(self.numeration_path)
+
+            # update the input tree
+            my_input = generate_svg_content(self.numeration[0])
+            # Load the SVG into QSvgWidget
+            self.input_tree.load(my_input.encode('utf-8'))
+
+            # run the first cycle for the input
+            self.outputs = proceed_cycle(self.numeration[0])
+
+            # Update the evaluation
+            self.update_eval()
+
+    def update_eval(self):
+        # Set the number of rows in the table
+        self.table_eval.setRowCount(len(self.outputs))
+
+        # Set the number of columns in the table
+        self.table_eval.setColumnCount(len(self.headers) + 1)
+    
+            # Set the headers for the table
+        headers = ['output'] + self.headers
+        self.table_eval.setHorizontalHeaderLabels(headers)
+    
+        # Populate the table
+        for row, node in enumerate(self.outputs):
+            # Populate the first column with the node name
+            name_item = QTableWidgetItem(node.to_linear())  # Assuming node.name is the attribute for the node's name
+            self.table_eval.setItem(row, 0, name_item)
+        
+            new_node = clone_tree(node)
+            # Populate the rest of the columns with the node's data
+            
+            data_dict = new_node.result_feats
+            data_dict['merge_cond'] = merge_condition(new_node) # check for merge condition
+            data_dict['label_cons'] = label_constraint(new_node) # check for label constraint
+            data_dict = empty_agreement(new_node, data_dict) # check for empty agreement
+            for col, key in enumerate(self.headers, start=1):  # Start from column 1 to skip the first column
+                value = data_dict.get(key, '')
+                item = QTableWidgetItem(str(value))
+                self.table_eval.setItem(row, col, item)
+    
+        # Resize columns to content
+        self.table_eval.resizeColumnsToContents()
 
     def show_tree_popup(self):
         # Generate SVG content in memory
-        svg_content = generate_svg_content(my_trial)
+        svg_content = generate_svg_content()
 
         # Create a QDialog for the tree popup
         dialog = QDialog(self)
@@ -381,3 +460,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
