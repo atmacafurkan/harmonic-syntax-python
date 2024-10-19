9cbf18b8975c7bf74db2c2743f09674be8ff1a3f
Furkan <furkanatmaca@outlook.com.tr>
2024-10-19 09:11:18 +0300

    update

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Harmonic Syntax Tabulator')
+        self.resize(1920,1080)
         self.initUI()
         
     def initUI(self):
